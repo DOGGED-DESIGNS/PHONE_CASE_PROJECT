5323ae1b818b88ed1c42643e96916da12fc048c1
dogged <uzoechijerry@gmail.com>
2024-10-18 06:27:01 +0100

    try

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -24,46 +24,48 @@ export async function POST(req: Request) {
       if (!event.data.object.customer_details?.email) {
         throw new Error("Missing user email");
       }
-      const session = event.data.object as Stripe.Checkout.Session;
-      const { userId, orderId } = session.metadata || {
-        userId: null,
-        orderId: null,
-      };
-      if (!userId || orderId) {
-        throw new Error("invalid request metadata");
-      }
+      // const session = event.data.object as Stripe.Checkout.Session;
+      // console.log('above is the session')
+      // console.log(session)
+      // const { userId, orderId } = session.metadata || {
+      //   userId: null,
+      //   orderId: null,
+      // };
+      // if (!userId || orderId) {
+      //   throw new Error("invalid request metadata");
+      // }
 
-      const billingAddress = session.customer_details!.address;
-      const shippingAddress = session.shipping_details!.address;
+      // const billingAddress = session.customer_details!.address;
+      // const shippingAddress = session.shipping_details!.address;
 
-      await db.order.update({
-        where: {
-          id: orderId!,
-        },
-        data: {
-          isPaid: true,
-          shippingAddress: {
-            create: {
-              name: session.customer_details!.name!,
-              city: shippingAddress!.city!,
-              country: shippingAddress!.country!,
-              postalCode: shippingAddress!.postal_code!,
-              street: shippingAddress!.line1!,
-              state: shippingAddress!.state!,
-            },
-          },
-          billingAddress: {
-            create: {
-              name: session.customer_details!.name!,
-              city: billingAddress!.city!,
-              country: billingAddress!.country!,
-              postalCode: billingAddress!.postal_code!,
-              street: billingAddress!.line1!,
-              state: billingAddress!.state!,
-            },
-          },
-        },
-      });
+      // await db.order.update({
+      //   where: {
+      //     id: orderId!,
+      //   },
+      //   data: {
+      //     isPaid: true,
+      //     shippingAddress: {
+      //       create: {
+      //         name: session.customer_details!.name!,
+      //         city: shippingAddress!.city!,
+      //         country: shippingAddress!.country!,
+      //         postalCode: shippingAddress!.postal_code!,
+      //         street: shippingAddress!.line1!,
+      //         state: shippingAddress!.state!,
+      //       },
+      //     },
+      //     billingAddress: {
+      //       create: {
+      //         name: session.customer_details!.name!,
+      //         city: billingAddress!.city!,
+      //         country: billingAddress!.country!,
+      //         postalCode: billingAddress!.postal_code!,
+      //         street: billingAddress!.line1!,
+      //         state: billingAddress!.state!,
+      //       },
+      //     },
+      //   },
+      // });
     }
 
     console.log(" i have exited the event section");
